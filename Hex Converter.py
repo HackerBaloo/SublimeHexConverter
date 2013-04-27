3cd57730911cb0f7e56ce5125ebcf41fff185d63
Björn Carlsson <b.r.carlsson@gmail.com>
2013-04-27 23:56:03 +0200

    Initial commit

---
+++ Hex Converter.py
@@ -0,0 +1,86 @@
+import os
+import subprocess
+import sublime
+import sublime_plugin
+
+
+def get_separator():
+    settings = sublime.load_settings("Hex Converter.sublime-settings")
+    return settings.get('separator')
+
+
+def convert(edit, view, prefix, base, in_place):
+    print('base: ', base, 'prefix: ', prefix, 'in_place: ', in_place)
+    selected_regions = view.sel()
+    for region in selected_regions:
+        value = view.substr(region)
+        print('selection: ', value)
+    for region in selected_regions:
+        value = view.substr(region)
+        print('selection: ', value)
+        if value.startswith(prefix):
+            result = str(int(value, base))
+        else:
+            if base == 16:
+                result = hex(int(value))
+            elif base == 8:
+                result = oct(int(value))
+            elif base == 2:
+                result = bin(int(value))
+            else:
+                sublime.error_message('Base not implemented: ', base)
+
+        print('result: ', result)
+        if in_place:
+            view.replace(edit, region, result)
+        else:
+            view.replace(edit, region, value + ' / ' + result)
+        return value, result
+
+
+class Base(sublime_plugin.TextCommand):
+    def __init__(self, view):
+        self.view = view
+
+    def run(self, edit):
+        value, result = convert(edit, self.view, self.prefix, self.base, 'InPlace' in type(self).__name__)
+        sublime.status_message(self.status_message + value + "=" + result)
+
+
+class ConvertToFromHex(Base):
+    def __init__(self, view):
+        super(ConvertToFromHex, self).__init__(view)
+        self.status_message = "Converted to / from hexadecimal: "
+        self.prefix = '0x'
+        self.base = 16
+
+
+class ConvertToFromHexInPlace(ConvertToFromHex):
+    def __init__(self, view):
+        super(ConvertToFromHexInPlace, self).__init__(view)
+
+
+class ConvertToFromOctal(Base):
+    def __init__(self, view):
+        super(ConvertToFromOctal, self).__init__(view)
+        self.status_message = "Converted to / from octal: "
+        self.prefix = '0'
+        self.base = 8
+
+
+class ConvertToFromOctalInPlace(ConvertToFromOctal):
+    def __init__(self, view):
+        super(ConvertToFromOctalInPlace, self).__init__(view)
+
+
+class ConvertToFromBinary(Base):
+    def __init__(self, view):
+        super(ConvertToFromBinary, self).__init__(view)
+        self.status_message = "Converted to / from binary: "
+        self.prefix = '0b'
+        self.base = 2
+
+
+class ConvertToFromBinaryInPlace(ConvertToFromBinary):
+    def __init__(self, view):
+        super(ConvertToFromBinaryInPlace, self).__init__(view)
--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-SublimeHexConverter
-===================
+Hex Converter
+===========================
+Author: Björn Carlsson
+License: MIT
+https://github.com/HackerBaloo/SublimeHexConverter
 
-Sublime plugin that converts numbers to and from Hexadecimal, Octal and Binary
+About
+-----
+A sublime plugin that converts the selected number to or from decimal
+to binary / octal / hexadecimal
+It can be done as in place replace of the selected number or the
+conversion could be added after the number with a separator
+
+Usage
+-----
+All you need to do is to press 'ctrl+shift+o' and 
+Hex Converter will open the selected file in Total Commander 
+or another app if you have changed the  settings
+
+    Hex conversion
+        Selection   Result          TypOfConversion
+        32          32 / 0x20       Normal
+        0x20        0x20 / 32       Normal
+        32          0x20            InPlace
+        0x20        32              InPlace
+
+    Octal conversion
+        Selection   Result          TypOfConversion
+        32          32 / 040        Normal
+        040         040 / 32        Normal
+        32          040             InPlace
+        040         32              InPlace
+
+    Binary conversion
+        Selection   Result          TypOfConversion
+        32          32 / 0b100000   Normal
+        0b100000    0b100000 / 32   Normal
+        32          0b100000        InPlace
+        0b100000    32              InPlace
+
+Configuration
+-------------
+separator, defaults to ' / '
+
+Installation
+------------
+**With Package Control:** The easiest way to install Hex Converter is
+by using the [Package Control plugin]
+(http://wbond.net/sublime_packages/package_control).
+
+**Without Git:** Download the latest source from 
+[GitHub](https://github.com/HackerBaloo/SublimeHexConverter) and copy 
+the Hex Converter folder to your Sublime Text 2 "Packages" directory.
+
+**With Git:** Clone the repository in your Sublime Text 2 "Packages" directory:
+
+    git clone https://github.com/HackerBaloo/SublimeHexConverter
+
+The "Packages" directory can be found in the following locations:
+
+* OS X:
+
+        ~/Library/Application Support/Sublime Text 2/Packages/
+
+* Linux:
+
+        ~/.config/sublime-text-2/Packages/
+
+* Windows:
+
+        %APPDATA%/Sublime Text 2/Packages/
+
